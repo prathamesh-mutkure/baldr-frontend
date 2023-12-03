ad5fbd0effb8f615f509fa5d2112a3c7f0da0c0f
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-12-04 01:04:26 +0530

    Added Dark Mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-
+import { ThemeProvider } from "@/components/theme-provider";
 import HomePage from "./pages/home";
 import DashboardPage from "./pages/dashboard";
 
 function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <HashRouter>
         <Routes>
           <Route path={"/"} element={<HomePage />} />
           <Route path={"/dashboard/"} element={<DashboardPage />} />
         </Routes>
       </HashRouter>
-    </>
+    </ThemeProvider>
   );
 }
 
